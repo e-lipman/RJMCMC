752a34d5be14aef9ba0e74f6e862797d20e39320
Erin Lipman <erlipman81@gmail.com>
2022-03-13 15:01:08 -0700

    refactor: move acceptance rate calc to functions

--- sandbox_real_data.R
+++ sandbox_real_data.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(yaml)
+library(tictoc)
 
 configs <- read_yaml("config.yaml")
 
@@ -8,7 +9,7 @@ for (f in list.files("functions")){
   source(file.path("functions",f))
 }
 
-dat_name = "galaxy"
+dat_name = "enzyme"
 
 if (dat_name == "enzyme"){
   library(BNPdensity)
@@ -24,16 +25,18 @@ if (dat_name == "enzyme"){
 
 hist(y, breaks=50)
 
-k_init <- 3
+k_init <- 2
 w_init <- rep(1/k_init, k_init)
 z_init <- ceiling(k_init*(1:length(y))/length(y))
 mu_init <- quantile(y, seq(0,1,length=k_init))
-sig2_init <- rep(var(y), k_init)
+sig2_init <- rep(max(y)-min(y), k_init)
 
 # test run
+tic()
 set.seed(12345)
 out <- run_mcmc(y, k_init, w_init, z_init, mu_init, sig2_init,
                 sweeps=1000, burn=1000)
+toc()
 
 # interpret results
 
@@ -41,24 +44,11 @@ out <- run_mcmc(y, k_init, w_init, z_init, mu_init, sig2_init,
 prop.table(table(out$k))
 plot(out$k, type="l")
 
-# acceptance fpr split and combine
-accept_stats <- tibble(label = c("split","combine",
-                                 "birth","death"),
-                       res = list(out$accept_split, 
-                                  out$accept_combine,
-                                  out$accept_birth,
-                                  out$accept_death)) %>%
-  mutate(x=map(res, sum),
-         n=map(res, length),
-         p=map(res, mean)) %>%
-  select(-res) %>%
-  unnest(c(x,n,p))
-accept_stats
+## Predictive densities (in progress)
+plot_posterior_densities(y, out, k=1:9)
 
-accept_stats %>%
-  mutate(group=rep(c("e","f"), each=2)) %>%
-  group_by(group) %>%
-  summarise(x=sum(x), n=sum(n), p=x/n)
+# acceptance fpr split and combine
+get_accept_stats(out, agg=T)
 
 ## posterior means
 post_means_by_k <-
@@ -66,13 +56,10 @@ post_means_by_k <-
   mutate(post_means = map(k, post_means_fixed_k, out=out)) %>%
   unnest(post_means)
 
-post_means_by_k %>% filter(k==5)
+post_means_by_k %>% filter(k==2)
 
 ## cluster plot
 make_cluster_plot(out)
-
-## Predictive densities (in progress)
-plot_posterior_densities(y, out, k=1:9)
 
 ## How often are components empty?
 z_count <- apply(out$z, 1, function(x){length(unique(x))})
