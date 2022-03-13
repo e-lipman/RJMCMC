0a842bb8185af29570f116d140b9279f017fc580
Erin Lipman <erlipman81@gmail.com>
2022-03-12 19:24:47 -0800

    feat: import all datasets and begin testing on data

--- sandbox_real_data.R
+++ sandbox_real_data.R
@@ -8,10 +8,24 @@ for (f in list.files("functions")){
   source(file.path("functions",f))
 }
 
-y <- sort(MASS::galaxies)
+dat_name = "galaxy"
 
-k_init <- 10
-w_init <- 1/k_init
+if (dat_name == "enzyme"){
+  library(BNPdensity)
+  data(enzyme)
+  y <- sort(enzyme)
+} else if (dat_name == "acidity"){
+  library(multimode)
+  data(acidity)
+  y <- sort(acidity)
+} else if (dat_name == "galaxy"){
+  y <- sort(MASS::galaxies)/1000
+}
+
+hist(y, breaks=50)
+
+k_init <- 3
+w_init <- rep(1/k_init, k_init)
 z_init <- ceiling(k_init*(1:length(y))/length(y))
 mu_init <- quantile(y, seq(0,1,length=k_init))
 sig2_init <- rep(var(y), k_init)
@@ -41,23 +55,27 @@ accept_stats <- tibble(label = c("split","combine",
   unnest(c(x,n,p))
 accept_stats
 
+accept_stats %>%
+  mutate(group=rep(c("e","f"), each=2)) %>%
+  group_by(group) %>%
+  summarise(x=sum(x), n=sum(n), p=x/n)
+
 ## posterior means
 post_means_by_k <-
   tibble(k=unique(out$k)) %>%
   mutate(post_means = map(k, post_means_fixed_k, out=out)) %>%
   unnest(post_means)
 
-post_means_by_k %>% filter(k==3)
-
-post_means_by_k %>%
-  filter(mu==max(mu) | mu==min(mu))
+post_means_by_k %>% filter(k==5)
 
 ## cluster plot
 make_cluster_plot(out)
 
-1## Predictive densities (in progress)
-plot_posterior_densities(y, out, k=3:5)
+## Predictive densities (in progress)
+plot_posterior_densities(y, out, k=1:9)
 
 ## How often are components empty?
 z_count <- apply(out$z, 1, function(x){length(unique(x))})
 table(out$k, out$k-z_count)
+mean(out$k-z_count)
+
