697c08002c540e3ff80652f2384eec8edc5b9557
Erin Lipman <erlipman81@gmail.com>
2022-03-15 14:47:44 -0700

    clean: Clean up to level script

--- sandbox_real_data.R
+++ sandbox_real_data.R
@@ -9,7 +9,7 @@ for (f in list.files("functions")){
   source(file.path("functions",f))
 }
 
-dat_name = "enzyme"
+dat_name = configs$dat_name
 
 if (dat_name == "enzyme"){
   library(BNPdensity)
@@ -21,34 +21,52 @@ if (dat_name == "enzyme"){
   y <- sort(acidity)
 } else if (dat_name == "galaxy"){
   y <- sort(MASS::galaxies)/1000
+} else{
+  stop("Bad dat_name")
 }
 
 hist(y, breaks=50)
 
-k_init <- 2
-w_init <- rep(1/k_init, k_init)
-z_init <- ceiling(k_init*(1:length(y))/length(y))
-mu_init <- quantile(y, seq(0,1,length=k_init))
-sig2_init <- rep(max(y)-min(y), k_init)
-
-# test run
+# run sampler
 tic()
-set.seed(12345)
-out <- run_mcmc(y, k_init, w_init, z_init, mu_init, sig2_init,
-                sweeps=1000, burn=1000)
+set.seed(71835)
+out <- run_rjmcmc(y, sweeps=1000, burn=1000)
 toc()
 
-# interpret results
+# results
 
 ## posterior for k
-prop.table(table(out$k))
-plot(out$k, type="l")
+prop.table(table(out$k))      # posterior for k
+plot(out$k, type="l")         # traceploft for k
 
-## Predictive densities (in progress)
-plot_posterior_densities(y, out, k=1:9)
+if (configs$save){
+  k_post <- tibble(k=names(table(out$k)),
+                   prob=table(out$k))
+  saveRDS(k_post,
+          file.path("results", paste0(dat_name, "_kpost.RDS")))
+}
 
-# acceptance fpr split and combine
-get_accept_stats(out, agg=T)
+## cluster plot
+cluster_plot <- make_cluster_plot(out)
+cluster_plot
+
+if (configs$save){
+  saveRDS(cluster_plot,
+          file.path("results", paste0(dat_name, "_cluster.RDS")))
+  ggsave(width=3, height = fig_ratio,
+         file.path("figures", paste0(dat_name, "_cluster.jpeg")))
+}
+
+## posterior predictive densities
+pp_plot <- plot_posterior_densities(y, out, k=1:6)
+pp_plot
+
+if (configs$save){
+  saveRDS(pp_plot,
+          file.path("results", paste0(dat_name, "_pp.RDS")))
+  ggsave(width=3, height = fig_ratio,
+       file.path("figures", paste0(dat_name, "_pp.jpeg")))
+}
 
 ## posterior means
 post_means_by_k <-
@@ -56,13 +74,20 @@ post_means_by_k <-
   mutate(post_means = map(k, post_means_fixed_k, out=out)) %>%
   unnest(post_means)
 
-post_means_by_k %>% filter(k==2)
+k_mode <- names(table(out$k))[which.max(table(out$k))]
+post_means_by_k %>% filter(k==k_mode)
 
-## cluster plot
-make_cluster_plot(out)
+## acceptance rates and empty components
+accept_stats <- get_accept_stats(out, agg=T) 
+accept_stats                                  # acceptance rates
 
-## How often are components empty?
 z_count <- apply(out$z, 1, function(x){length(unique(x))})
-table(out$k, out$k-z_count)
-mean(out$k-z_count)
+mean(out$k-z_count)                           # empty components
 
+if (configs$save){
+  mix_stats <- tibble(accept_e = accept_stats$p[1], 
+                      accept_f = accept_stats$p[2], 
+                      num_empty = mean(out$k-z_count))
+  saveRDS(mix_stats,
+          file.path("results", paste0(dat_name, "_stats.RDS")))
+}
